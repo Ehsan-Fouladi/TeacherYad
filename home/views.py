4f4e7aa4cee4c6a99f8fa2515cb2f97779e19642
Ehsan-Fouladi <fooehan3@gmail.com>
2023-07-03 23:18:37 +0330

    Home

--- home/views.py
+++ home/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import ListView, TemplateView
+from django.shortcuts import render, redirect
+from django.views.generic import ListView
 from .models import Support
 from teacher.models import Teacher
 from aboutus.models import Home
-
+from django.contrib import messages
 
 class HomeView(ListView):
 
@@ -18,4 +18,5 @@ class HomeView(ListView):
             email = request.POST.get('email')
             message = request.POST.get('message')
             Support.objects.create(name=name, email=email, message=message)
-            return render(request, "home/support.html")
+            messages.success(self.request, "!درخواست شما با موفقیت ثبت شد")
+            return redirect("home:home")
